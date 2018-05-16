a4381ae375c01e23cc121871a66a65d78c3449db
gavin-love <1co.loco@oprotonmail.ch>
2018-05-16 17:57:04 -0600

    Add dictionary to suggestions

---
+++ test/dictionary-test.js
@@ -0,0 +1,45 @@
+import fs from 'fs';
+import { expect } from 'chai';
+import Trie from '../lib/Trie.js';
+
+describe('Trie', () => {
+  let trie;
+  const text = "/usr/share/dict/words";
+  const dictionary = fs.readFileSync(text).toString().trim().split('\n');
+
+  beforeEach(() => {
+    trie = new Trie();
+  });
+
+  describe('Dictionary', () => {
+  
+    it('should count words in the dicitonary', () => {
+      trie.populate(dictionary);
+      trie.countComplete();
+  
+      expect(trie.counter).to.equal(234371);
+    });
+    
+    it('should suggest a word based on a prefix', () => {
+      trie.populate(dictionary);
+      trie.suggest('world');
+  
+      expect(trie.suggestions).to.deep.equal(['worlded', 'worldful', 'worldish', 'worldless', 'worldlet',
+        'worldlike', 'worldlily', 'worldliness', 'worldling', 'worldly', 'worldmaker', 'worldmaking', 'worldproof', 'worldquake', 'worldward', 'worldwards', 'worldway', 'worldy']);
+    });
+
+    it('should suggest more words based on a prefix', () => {
+      trie.populate(dictionary);
+
+      trie.suggest('sad');
+
+      expect(trie.suggestions).to.deep.equal(['sadachbia', 'sadalmelik', 'sadalsuud', 'sadden', 'saddening', 'saddeningly', 'saddik', 'saddirham', 'saddish', 'saddle', 'saddleback', 'saddlebag',
+        'saddlebow', 'saddlecloth', 'saddled', 'saddleleaf', 'saddleless', 'saddlelike', 'saddlenose',
+        'saddler', 'saddlery', 'saddlesick', 'saddlesore', 'saddlesoreness', 'saddlestead', 'saddletree',
+        'saddlewise', 'saddling', 'sadducaic', 'sadducean', 'sadducee', 'sadduceeism', 'sadduceeist',
+        'sadducism', 'sadducize', 'sade', 'sadh', 'sadhe', 'sadhearted', 'sadhu', 'sadic', 'sadie',
+        'sadiron', 'sadism', 'sadist', 'sadistic', 'sadistically', 'sadite', 'sadly', 'sadness', 'sado',
+        'sadomasochism', 'sadr']);
+    });
+  });
+});
