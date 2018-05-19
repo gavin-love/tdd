a6a8bd4386d9413e61b1e0a3b9853b34de7063fb
gavin-love <1co.loco@oprotonmail.ch>
2018-05-18 20:03:31 -0600

    Add testing for algorithms

---
+++ test/bubblesort-test.js
@@ -0,0 +1,46 @@
+import bubbleSort from '../lib/bubble-sort.js';
+import arrayGenerator from '../lib/array-generator.js';
+
+var chai = require('chai');
+var expect = chai.expect;
+
+describe('bubbleSort', function () {
+
+  it('should be a function', () => {
+
+    expect(bubbleSort).to.exist;
+  });
+
+  it('should sort an array of 5 numbers', () => {
+    let array;
+
+    array = arrayGenerator(5);
+ 
+    let expected = array.sort();
+    let actual = bubbleSort(array);
+
+    expect(expected).to.deep.equal(actual);
+  });
+
+  it('should sort an array of 5000 numbers', () => {
+    let array;
+
+    array = arrayGenerator(5000);
+
+    let expected = array.sort();
+    let actual = bubbleSort(array);
+
+    expect(expected).to.deep.equal(actual);
+  });
+
+  it('should sort a large array of numbers', () => {
+    let array;
+
+    array = arrayGenerator(35000);
+
+    let expected = array.sort();
+    let actual = bubbleSort(array);
+
+    expect(expected).to.deep.equal(actual);
+  });
+});
